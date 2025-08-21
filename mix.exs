13fcb4f63645091492844311311a03350c07ea24
Rick Clare <rickclare@users.noreply.github.com>
2025-08-21 15:11:18 +0100

    upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -81,10 +81,10 @@ defmodule Operations.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"},
+      {:bandit, "~> 1.8"},
       #
       {:credo, "~> 1.7.12", only: [:dev, :test], runtime: false},
-      {:styler, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:styler, "~> 1.7", only: [:dev, :test], runtime: false},
       {:tailwind_formatter, "~> 0.4.2", only: [:dev, :test], runtime: false}
     ]
   end
