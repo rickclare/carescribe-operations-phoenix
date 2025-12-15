a0622107c888de55e904d3cda79a9e29092ea0e3
Rick Clare <rickclare@users.noreply.github.com>
2025-12-15 11:14:45 +0000

    update dependencies, again3

--- mix.exs
+++ mix.exs
@@ -90,7 +90,7 @@ t       .setup": :e2e,
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.8"},
+      {:bandit, "~> 1.9"},
       {:ueberauth_google, "~> 0.10"},
       #
       {:credo, "~> 1.7.12", only: [:dev, :test], runtime: false},
